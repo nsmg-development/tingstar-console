2d46bba82bfb4f448aed104ae7eaa4080fcfb3a7
SaemheeKim <60594031+SaemheeKim@users.noreply.github.com>
2021-09-03 15:39:37 +0900

    FEATURE-CUR-60 console-card-view (#4)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,69 @@
 <template>
     <v-container fluid>
         <Breadcrumbs :breadcrumbs="breadcrumbs"/>
-        <v-data-table
-            :headers="headers"
-            :items="items"
-            :loading="loading"
-            item-key="id"
-            show-select
-            class="row-pointer"
-            @click:row="handleClick"
-        >
-            <template v-slot:item.storage_thumbnail_url="{ item }">
-                <v-img
-                    max-height="100"
-                    max-width="200"
-                    :src="item.storage_thumbnail_url"
-                ></v-img>
-            </template>
-            <template v-slot:item.state="{ item }">
-                <v-select
-                    v-model="item.state"
-                    :items="state_items"
-                    item-text="state"
-                    item-value="value"
-                    color="#E00051"
-                    @change="changeState(item.id)"
-                    v-on:click.stop
-                ></v-select>
-            </template>
-            <template v-slot:item.has_media="{ item }">
-                <v-chip :color="color" class="white--text">
-                    {{ item.has_media }}
-                </v-chip>
-            </template>
-        </v-data-table>
+        <!--        <v-data-table-->
+        <!--            :headers="headers"-->
+        <!--            :items="items"-->
+        <!--            :loading="loading"-->
+        <!--            item-key="id"-->
+        <!--            show-select-->
+        <!--            class="row-pointer"-->
+        <!--            @click:row="handleClick"-->
+        <!--        >-->
+        <!--            <template v-slot:item.storage_thumbnail_url="{ item }">-->
+        <!--                <v-img-->
+        <!--                    max-height="100"-->
+        <!--                    max-width="200"-->
+        <!--                    :src="item.storage_thumbnail_url"-->
+        <!--                ></v-img>-->
+        <!--            </template>-->
+        <!--            <template v-slot:item.state="{ item }">-->
+        <!--                <v-select-->
+        <!--                    v-model="item.state"-->
+        <!--                    :items="state_items"-->
+        <!--                    item-text="state"-->
+        <!--                    item-value="value"-->
+        <!--                    color="#E00051"-->
+        <!--                    @change="changeState(item.id)"-->
+        <!--                    v-on:click.stop-->
+        <!--                ></v-select>-->
+        <!--            </template>-->
+        <!--            <template v-slot:item.has_media="{ item }">-->
+        <!--                <v-chip :color="color" class="white&#45;&#45;text">-->
+        <!--                    {{ item.has_media }}-->
+        <!--                </v-chip>-->
+        <!--            </template>-->
+        <!--        </v-data-table>-->
+        <v-row>
+            <v-col
+                cols="12" sm="6" md="4" lg="3"
+                v-for="(item, i) in items" :key="i"
+            >
+                <v-card>
+                    <v-img
+                        class="row-pointer"
+                        height="200px"
+                        :src="item.storage_thumbnail_url"
+                        @click="handleClick(item)"
+                    >
+                    </v-img>
+                    <v-card-title>
+                        {{ item.keyword }}
+                    </v-card-title>
+                    <v-card-subtitle>
+                        {{ item.platform }}
+                        <v-select
+                            v-model="item.state"
+                            :items="state_items"
+                            item-text="state"
+                            item-value="value"
+                            @change="changeState(item.id)"
+                            style="width: 70px"
+                        ></v-select>
+                    </v-card-subtitle>
+                </v-card>
+            </v-col>
+        </v-row>
         <v-pagination
             class="mt-5"
             v-model="page"
@@ -63,18 +93,6 @@ export default {
                     href: '/',
                 },
             ],
-            headers: [
-                {text: 'NO.', value: 'no', filterable: false},
-                {text: '썸네일', value: 'storage_thumbnail_url', filterable: false},
-                {text: '플랫폼', value: 'platform', sortable: false},
-                {text: '타입', value: 'type', sortable: false},
-                {text: '키워드', value: 'keyword', sortable: false},
-                {text: '채널', value: 'channel', sortable: false},
-                {text: '원본 URL', value: 'url', sortable: false},
-                {text: '게시 날짜', value: 'date', sortable: false},
-                {text: '게시 여부', value: 'state', sortable: false},
-                {text: '미디어', value: 'has_media', sortable: false},
-            ],
             items: [],
             state_items: [
                 {state: '미설정', value: 0},
@@ -103,7 +121,7 @@ export default {
                 .then(res => {
                     if (res.data.data.articles.length > 0) {
                         res.data.data.articles.map((item, index) => {
-                            if(item.has_media == true) {
+                            if (item.has_media == true) {
                                 media = 'Y'
                                 this.color = '#E00051'
                             } else {
@@ -154,7 +172,7 @@ export default {
 </script>
 
 <style scoped>
-.row-pointer >>> tbody tr :hover {
+.row-pointer :hover {
     cursor: pointer;
 }
 </style>
